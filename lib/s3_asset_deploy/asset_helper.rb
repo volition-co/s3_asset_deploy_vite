2c47c3f1e2e69661b2d4b70073e1ca9992e32298
Arjun Singh <arjun810@gmail.com>
2023-12-12 12:17:13 -0800

    move vite fingerprint

--- lib/s3_asset_deploy/asset_helper.rb
+++ lib/s3_asset_deploy/asset_helper.rb
@@ -16,4 +16,26 @@ class S3AssetDeploy::AssetHelper
     return "application/json" if extension == "map"
     MIME::Types.type_for(extension).first
   end
+
+  # Vite hashes should be configured to start with the
+  # vite_hash_prefix (e.g. -vtash). Return the filename without the
+  # hash, but keep the extension.
+  #
+  # Turn assets/uppy-vthash0opbZYF5.css into assets/uppy-vite.css
+  #
+  # We add -vite because some assets may be managed by both Sprockets
+  # and Vite.
+  # If the asset is marked as dynamic with the vite_dynamic_flag (e.g.
+  # -vtdynamic), then we don't remove the hash.
+  def remove_fingerprint_vite(path, vite_hash_prefix, vite_dynamic_flag)
+      if path.include?(vite_dynamic_flag)
+          return path
+      elsif path.include?(vite_hash_prefix)
+          *prefix, extension = path.split(".")
+          filename = prefix.join(".")
+          return filename.gsub(/#{vite_hash_prefix}.*/,'-vite') + "." + extension
+      else
+          self.remove_fingerprint(path)
+      end
+  end
 end
--- lib/s3_asset_deploy/vite_local_asset_collector.rb
+++ lib/s3_asset_deploy/vite_local_asset_collector.rb
@@ -1,10 +1,13 @@
 class ViteLocalAssetCollector < S3AssetDeploy::RailsLocalAssetCollector
-    def initialize(remove_fingerprint: nil, vite_hash_prefix: '-vthash', vite_dynamic_flag: '-vtdynamic')
+    def initialize(remove_fingerprint: nil, vite_hash_prefix: nil, vite_dynamic_flag: nil)
         super
         remove_fingerprint = ->(path) { self.remove_fingerprint(path) } if remove_fingerprint.nil?
         @remove_fingerprint = remove_fingerprint
         @vite_hash_prefix = vite_hash_prefix
         @vite_dynamic_flag = vite_dynamic_flag
+
+        raise "Vite hash prefix is required" unless @vite_hash_prefix
+        raise "Vite dynamic flag is required" unless @vite_dynamic_flag
     end
 
     def assets
@@ -33,25 +36,7 @@ class ViteLocalAssetCollector < S3AssetDeploy::RailsLocalAssetCollector
         vite_paths.uniq.map{ |path| S3AssetDeploy::RailsLocalAsset.new(path, remove_fingerprint: @remove_fingerprint) }
     end
 
-    # Vite hashes should be configured to start with the
-    # vite_hash_prefix (e.g. -vtash). Return the filename without the
-    # hash, but keep the extension.
-    #
-    # Turn assets/uppy-vthash0opbZYF5.css into assets/uppy-vite.css 
-    
-    # We add -vite because some assets may be managed by both Sprockets
-    # and Vite.
-    # If the asset is marked as dynamic with the vite_dynamic_flag (e.g.
-    # -vtdynamic), then we don't remove the hash.
     def remove_fingerprint(path)
-        if path.include?(@vite_dynamic_flag)
-            return path
-        elsif path.include?(@vite_hash_prefix)
-            *prefix, extension = path.split(".")
-            filename = prefix.join(".")
-            return filename.gsub(/#{@vite_hash_prefix}.*/,'-vite') + "." + extension
-        else
-            S3AssetDeploy::AssetHelper.remove_fingerprint(path)
-        end
+        S3AssetDeploy::AssetHelper.remove_fingerprint_vite(path, @vite_hash_prefix, @vite_dynamic_flag)
     end
 end
